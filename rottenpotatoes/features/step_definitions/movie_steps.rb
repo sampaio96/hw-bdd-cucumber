8ae8517207d07a04e994b0ffa2294d55d0b5ba87
Gonçalo Sampaio <gsampaio@wesleyan.edu>
2016-02-25 20:27:02 +0000

    corrected bug with regex

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -1,12 +1,12 @@
 # Add a declarative step here for populating the DB with movies.
 
+value = 0
 Given /the following movies exist/ do |movies_table|
+  value = 0
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
-    Movie.create!(movie)
+      Movie.create(movie)
+      value += 1
   end
-  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -14,18 +14,17 @@ end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
-  #  page.content  is the entire content of the page as a string.
-  page.body.should =~ /#{e1}.*#{e2}/m
+  # puts page.body
+  match = /#{e1}.*#{e2}/m =~ page.body
+  assert !match.nil?
 end
 
-Then /I should see (all|none) of the movies/ do |selection|
-  total_movies = page.all("tbody#movielist tr").count
-  puts "Total movies are #{total_movies}"
-  if selection == "all"
-    total_movies.should == 10
-  elsif selection == "none"
-    total_movies.should == 0
-  end
+Then /I should see all of the movies/ do
+  page.should have_css("table#movies tbody tr",:count => value.to_i)
+end
+
+Then /I should not see all of the movies/ do
+  page.should have_no_css("table#movies tbody tr")
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -33,19 +32,14 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  
-  uncheck = "" unless uncheck == "un"
-  rating_array = rating_list.split(",")
-  rating_array.each do |rating|
-    rating = rating.strip
-    step %{I #{uncheck}check "ratings_#{rating}"}
+  rating_list.delete!("\"")
+  if uncheck.nil?
+    rating_list.split(',').each do |field|
+      check("ratings["+field.strip+"]")
+    end
+  else
+    rating_list.split(',').each do |field|
+      uncheck("ratings["+field.strip+"]")
+    end
   end
-end
-
-Then /the director of "(.*)" should be "(.*)"/ do |title, director|
-  page.find_by_id('main').find('h2').should have_content("#{title}") and
-  page.find_by_id('details').should have_content("#{director}")
 end
