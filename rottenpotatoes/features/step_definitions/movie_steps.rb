964b6a7a68a83396a198d6658c092c28e439f812
Gonçalo Sampaio <gsampaio@wesleyan.edu>
2016-02-25 19:57:20 +0000

    Finished hw3

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,9 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  fail "Unimplemented"
+  #flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -13,8 +14,18 @@ end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+  #  page.content  is the entire content of the page as a string.
+  page.body.should =~ /#{e1}.*#{e2}/m
+end
+
+Then /I should see (all|none) of the movies/ do |selection|
+  total_movies = page.all("tbody#movielist tr").count
+  puts "Total movies are #{total_movies}"
+  if selection == "all"
+    total_movies.should == 10
+  elsif selection == "none"
+    total_movies.should == 0
+  end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -25,10 +36,16 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  fail "Unimplemented"
+  
+  uncheck = "" unless uncheck == "un"
+  rating_array = rating_list.split(",")
+  rating_array.each do |rating|
+    rating = rating.strip
+    step %{I #{uncheck}check "ratings_#{rating}"}
+  end
 end
 
-Then /I should see all the movies/ do
-  # Make sure that all the movies in the app are visible in the table
-  fail "Unimplemented"
+Then /the director of "(.*)" should be "(.*)"/ do |title, director|
+  page.find_by_id('main').find('h2').should have_content("#{title}") and
+  page.find_by_id('details').should have_content("#{director}")
 end
